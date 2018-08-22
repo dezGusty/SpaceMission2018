457e42c7effab0b89ed2480d67e8c4508f071adc
Anca Moldovan <Anca.Moldovan@ctbav.ro>
2018-08-22 13:03:21 +0300

    Added initial template for the game

---
+++ Source/SpaceShipGame/SpaceShipGame.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "SpaceShipGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpaceShipGame, "SpaceShipGame" );
+
+DEFINE_LOG_CATEGORY(LogSpaceShipGame)
+ 
