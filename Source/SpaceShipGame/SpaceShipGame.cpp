7009a1d3779f3ac3773c4182e645e0c7b736723a
Irina Puscasu <irina.puscasu98@gmail.com>
2018-09-12 13:04:50 +0300

    Am creat o noua clasa de HUD si un game mode BP

--- Source/SpaceShipGame/SpaceShipGame.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
-
-#include "SpaceShipGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpaceShipGame, "SpaceShipGame" );
-
-DEFINE_LOG_CATEGORY(LogSpaceShipGame)
- 
